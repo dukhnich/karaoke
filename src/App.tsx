16d39026c5ed92907dba28bf4a94ff41867b2cff
Maryna Dukhnich <maryna.dukhnich@gmail.com>
2024-05-30 20:54:53 +0200

    changing current line

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,21 @@ import lyricText from "./lyrics-lines";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [line, setLine] = useState(-1);
+  const handleTimeUpdate = (time: number) => {
+    const index = lyricText.findIndex((l) => l.time >= time);
+    setLine(index === -1 ? lyricText.length - 1 : index - 1);
+  };
 
   return (
     <div className="container">
       <h1>Fools Garden: Lemon Tree</h1>
-      <Player src="fools-garden-lemon-tree.mp3" />
-      <Lyrics lyrics={lyricText} currentLineIndex={10} />
+      <Player
+        src="fools-garden-lemon-tree.mp3"
+        onChangeTime={handleTimeUpdate}
+        onEnded={() => setLine(-1)}
+      />
+      <Lyrics lyrics={lyricText} currentLineIndex={line} />
     </div>
   );
 }
--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -3,9 +3,11 @@ import { useState, useEffect, useRef } from "react";
 
 interface PlayerProps {
   src: string;
+  onChangeTime: (time: number) => void;
+  onEnded: () => void;
 }
 
-export const Player: React.FC<PlayerProps> = ({ src }) => {
+export const Player: React.FC<PlayerProps> = ({ src, onChangeTime, onEnded }) => {
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
   const audioRef = useRef<HTMLAudioElement>(null!);
 
@@ -16,10 +18,21 @@ export const Player: React.FC<PlayerProps> = ({ src }) => {
   const handlePlay = () => {
     setIsPlaying(!isPlaying);
   };
+  const stateHandler = (ev: React.ChangeEvent<HTMLAudioElement>) => {
+    const target = ev.target;
+    if (ev.type === "timeupdate") {
+      onChangeTime(target.currentTime);
+    }
+  };
 
   return (
     <>
-      <audio src={src} ref={audioRef}></audio>
+      <audio
+        src={src}
+        ref={audioRef}
+        onTimeUpdate={stateHandler}
+        onEnded={onEnded}
+      ></audio>
       <div className="player-controls">
         <button
           className={`play-button ${isPlaying ? "pause" : "play"}`}
